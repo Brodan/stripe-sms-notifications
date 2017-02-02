77cbfdeac6708cd8cb6f17ef8345eebd6a5a1cd4
brodan <christopher.hranj@gmail.com>
2017-02-02 00:27:54 -0500

    update source, add twilio, parse incoming stripe request

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 
+# Find these values at https://twilio.com/user/account
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = TwilioRestClient(account_sid, auth_token)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+
+@app.route("/", methods=['POST'])
+def send_sms():
+    event_json = request.get_json()
+    amount = event_json['data']['object']['amount']
+    location = event_json['data']['object']['source']['address_state']
+    message_body = "order for {}".format(amount)
+    message = client.messages.create(to=os.environ['PHONE_NUMBER'], from_=os.environ['TWILIO_NUMBER'],
+                                         body=message_body)
+    return '', 200
 
 
 if __name__ == "__main__":
