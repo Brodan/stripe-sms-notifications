e2c5debf21df6f3685a596ff7668a6a2a909cc1a
brodan <christopher.hranj@gmail.com>
2017-02-06 02:55:06 -0500

    rename route and remove location stuff for now

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ client = TwilioRestClient(account_sid, auth_token)
 
 
 @app.route("/", methods=['POST'])
-def send_sms():
+def receive_order():
     event_json = request.get_json()
     amount = event_json['data']['object']['amount']
-    location = event_json['data']['object']['source']['address_state']
     message_body = "Hey! Your shop just recieved an order for $" + \
-        str(amount) + " in " + str(location) + "."
+        str(amount) + "."
     message = client.messages.create(to=os.environ['PHONE_NUMBER'],
                                      from_=os.environ['TWILIO_NUMBER'],
                                      body=message_body)
